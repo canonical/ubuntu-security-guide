e73fcc766f8c310d56460387ed2c62c59b9a0af9
Eduardo Barretto <eduardo.barretto@canonical.com>
2022-09-06 17:04:31 +0200

    tools: generate_tailoring_file: Fix indentation

--- tools/generate_tailoring_file.py
+++ tools/generate_tailoring_file.py
@@ -7,7 +7,7 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -21,20 +21,23 @@ import re
 import lxml.etree as etree
 import sys
 
+
 def search_comment(raw_yaml, linenum):
-    result=None
-    while result == None:
+    result = None
+    while result is None:
         assert(linenum >= 0)
-        result=search_comment.prog.match(raw_yaml[linenum])
+        result = search_comment.prog.match(raw_yaml[linenum])
         if result:
             return raw_yaml[linenum]
         else:
             linenum -= 1
 search_comment.prog = re.compile("^\s*#+\s*((?:\d+\.)*\d+\.?|UBTU-.*)\s+")
 
+
 def process_comment(s):
     # Remove newline chars and the shebang
     return s.strip(' \n#')
+
 
 def print_xml_comment(comment, add_line_before):
     ret_buf = ""
@@ -42,6 +45,7 @@ def print_xml_comment(comment, add_line_before):
         ret_buf += "\n"
     ret_buf += f"<!-- { comment } -->\n"
     return ret_buf
+
 
 def process_var(doc, var, val):
     root = doc.getroot()
@@ -53,41 +57,46 @@ def process_var(doc, var, val):
 
     return (Exception("No value found for variable { var }!"), None)
 
+
 def print_xml_var(err, var, pval):
-    if err != None:
+    if err is not None:
         str_err = str(err)
         print(f"<!-- Could not extract value from variable { var } ! Error: { str_err } -->")
     else:
         return f'<xccdf:set-value idref="{ var }">{ pval }</xccdf:set-value>\n'
 
-# If the raw rule name contains a '!' character, returns the name without it and return flag select as False
+
+# If the raw rule name contains a '!' character, returns the name
+# without it and return flag select as False
 def process_rule(rule):
     if rule.startswith("!"):
         return (rule.lstrip('!'), False)
     return (rule, True)
 
+
 def print_xml_rule(rule, is_selected):
     return f'<xccdf:select idref="{ rule }" selected="' + str(is_selected).lower() + '"/>\n'
 
+
 def create_tailoring_file(parsed_yaml, raw_yaml, xccdf_doc):
-    yaml_sel=parsed_yaml["selections"]
-    prog=re.compile('^([^=]+)=(.*)$') # Regexp to fetch variable lines
-    prev_linenum=-1
+    yaml_sel = parsed_yaml["selections"]
+    prog = re.compile('^([^=]+)=(.*)$')  # Regexp to fetch variable lines
+    prev_linenum = -1
     ret_buf = []
     for i in range(len(yaml_sel)):
         ret_item = ""
-        linenum=yaml_sel.lc.data[i][0]
+        linenum = yaml_sel.lc.data[i][0]
         # If it's the first linenum of a block, get the comment
         if prev_linenum != (linenum - 1):
-            comment_line = search_comment(raw_yaml, linenum - 1) # Start searching the previous line
+            comment_line = search_comment(raw_yaml, linenum - 1)  # Start searching the previous line
             pc = process_comment(comment_line)
             if prev_linenum != -1:
                 ret_item += print_xml_comment(pc, True)
-            else: # First line doesn't need spaces
+            else:  # First line doesn't need spaces
                 ret_item += print_xml_comment(pc, False)
 
         # Now line can be a variable assignment or a rule line
-        result=prog.match(yaml_sel[i])
+        result = prog.match(yaml_sel[i])
 
         if result:
             # Real value must be extract from XCCDF file
@@ -99,7 +108,7 @@ def create_tailoring_file(parsed_yaml, raw_yaml, xccdf_doc):
             prule, is_selected = process_rule(yaml_sel[i])
             ret_item += print_xml_rule(prule, is_selected)
 
-        prev_linenum=linenum
+        prev_linenum = linenum
         re_match = re.search('^[\n]?<!-- (.+?) ', ret_item)
         if re_match is not None:
             ret_buf.append(ret_item)
@@ -108,20 +117,22 @@ def create_tailoring_file(parsed_yaml, raw_yaml, xccdf_doc):
 
     return ret_buf
 
+
 def process_profile_file(yaml_path):
-    parsed_yaml=None
-    raw_yaml=None
+    parsed_yaml = None
+    raw_yaml = None
     try:
         with open(yaml_path, 'r') as yaml_file:
-            parsed_yaml=ruamel.yaml.round_trip_load(yaml_file)
+            parsed_yaml = ruamel.yaml.round_trip_load(yaml_file)
             yaml_file.seek(0)
-            raw_yaml=yaml_file.readlines()
+            raw_yaml = yaml_file.readlines()
 
-    except OSError as err:
+    except OSError:
         print("Could not open profile file", file=sys.stderr)
         sys.exit(1)
 
-    return raw_yaml,parsed_yaml
+    return raw_yaml, parsed_yaml
+
 
 def merge_tailoring_data(extended_data, parent_data):
     extended_data_rules = []
@@ -144,9 +155,11 @@ def merge_tailoring_data(extended_data, parent_data):
 
     return parent_data
 
+
 def print_tailoring_array(tailoring_data):
     for i in range(len(tailoring_data)):
         print(tailoring_data[i])
+
 
 USAGE = f"Usage: python {sys.argv[0]} <Profile file path> <XCCDF file path>"
 if __name__ == '__main__':
@@ -162,7 +175,7 @@ if __name__ == '__main__':
     xccdf_doc = None
     try:
         xccdf_doc = etree.parse(xccdf_path)
-    except etree.XMLSyntaxError as err:
+    except etree.XMLSyntaxError:
         print("Could not process XCCDF file", file=sys.stderr)
         sys.exit(1)
 
@@ -175,6 +188,5 @@ if __name__ == '__main__':
         tailoring_data = merge_tailoring_data(tailoring_data, parent_tailoring_data)
 
     print_tailoring_array(tailoring_data)
-
 
     sys.exit(0)
