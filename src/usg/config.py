d3acc47b56e1817fbff7ce61bd951b3d9be0c0bd
Miha Purg <miha.purg@canonical.com>
2025-08-20 15:55:41 +0200

    Linting and minor refactoring

--- src/usg/config.py
+++ src/usg/config.py
@@ -1,42 +1,40 @@
-"""
-Config loader and default configuration
-"""
+"""Config loader and default configuration."""
 
-import os
 import argparse
 import configparser
 import logging
-from typing import Any, Mapping
+from collections.abc import Mapping
 from pathlib import Path
+from typing import Any
 
 from usg import constants
 
 logger = logging.getLogger()
 
 DEFAULT_CONFIG: Mapping[str, Mapping[str, Any]] = {
-        "cli": {
-            "log_file": constants.CLI_LOG_FILE,
-            "benchmark_version": constants.DEFAULT_BENCHMARK_VERSION,
-            "product": constants.DEFAULT_PRODUCT,
-            "fix_only_failed": constants.DEFAULT_FIX_ONLY_FAILED,
-            },
-        "openscap_backend": {
-            "audit_report": constants.OPENSCAP_REPORT_FILE,
-            "audit_results": constants.OPENSCAP_RESULTS_FILE,
-            "audit_log": constants.OPENSCAP_LOG_FILE,
-            "fix_script": constants.OPENSCAP_FIX_FILE,
-            "fix_log": constants.OPENSCAP_FIX_LOG_FILE,
-            "audit_oval_results": constants.OPENSCAP_OVAL_RESULTS_FILE,
-            "audit_oval_cpe_results": constants.OPENSCAP_OVAL_CPE_RESULTS_FILE,
-            "save_oval_results": constants.OPENSCAP_SAVE_OVAL_RESULTS,
-            },
-        }
+    "cli": {
+        "log_file": constants.CLI_LOG_FILE,
+        "benchmark_version": constants.DEFAULT_BENCHMARK_VERSION,
+        "product": constants.DEFAULT_PRODUCT,
+        "fix_only_failed": constants.DEFAULT_FIX_ONLY_FAILED,
+    },
+    "openscap_backend": {
+        "audit_report": constants.OPENSCAP_REPORT_FILE,
+        "audit_results": constants.OPENSCAP_RESULTS_FILE,
+        "audit_log": constants.OPENSCAP_LOG_FILE,
+        "fix_script": constants.OPENSCAP_FIX_FILE,
+        "fix_log": constants.OPENSCAP_FIX_LOG_FILE,
+        "audit_oval_results": constants.OPENSCAP_OVAL_RESULTS_FILE,
+        "audit_oval_cpe_results": constants.OPENSCAP_OVAL_CPE_RESULTS_FILE,
+        "save_oval_results": constants.OPENSCAP_SAVE_OVAL_RESULTS,
+    },
+}
+
 
 def load_config(
-        config_path: Path | str | None = None
-        ) -> configparser.ConfigParser:
-    """
-    Initialize and return config.
+    config_path: Path | str | None = None,
+) -> configparser.ConfigParser:
+    """Initialize and return config.
 
     Default config is loaded from DEFAULT_CONFIG and overridden with
     config from config_path, if provided.
@@ -46,6 +44,7 @@ def load_config(
 
     Returns:
         configparser.ConfigParser
+
     """
     logger.debug(f"Loading config from {config_path}")
 
@@ -55,11 +54,10 @@ def load_config(
         logger.debug(f"Loading config {config_path}")
         try:
             config.read(config_path)
-        except Exception as e:
+        except Exception as e:  # noqa: BLE001
             logger.error(
-                    f"Failed to load config {config_path}: {e}. "
-                    f"Using defaults."
-                    )
+                f"Failed to load config {config_path}: {e}. Using defaults.",
+            )
     else:
         logger.info(f"Config {config_path} does not exist. Using defaults.")
 
@@ -68,16 +66,16 @@ def load_config(
 
 
 def get_artifact_destination_path(
-        config: configparser.ConfigParser,
-        option: str,
-        timestamp: str,
-        cac_profile: str,
-        product: str,
-        ) -> Path:
-    """
-    Get destination path of a backend artifact file from the config.
-    
-    Replace placeholders to maintain backwards compatibility with legacy USG (PRODUCT, DATE and PROFILE_ID).
+    config: configparser.ConfigParser,
+    option: str,
+    timestamp: str,
+    cac_profile: str,
+    product: str,
+) -> Path:
+    """Get destination path of a backend artifact file from the config.
+
+    Replace placeholders to maintain backwards compatibility with legacy USG
+    (PRODUCT, DATE and PROFILE_ID).
     If the path is not absolute, prefix with STATE_DIR.
 
     Args:
@@ -86,17 +84,19 @@ def get_artifact_destination_path(
         timestamp: timestamp (YYYYMMDD.HHMM)
         cac_profile: profile id (e.g. cis_level1_server)
         product: product name (e.g. ubuntu2404)
-    
+
     Returns:
         Path
+
     """
-    logger.debug(f"Getting artifact destination path for {option} with profile {cac_profile} and timestamp {timestamp}")
+    logger.debug(
+        f"Getting artifact destination path for {option} "
+        f"with profile {cac_profile} and timestamp {timestamp}"
+    )
     path_str = config["openscap_backend"][option]
-    path = Path(path_str.format(
-        PRODUCT = product,
-        DATE = timestamp,
-        PROFILE_ID = cac_profile)
-        )
+    path = Path(
+        path_str.format(PRODUCT=product, DATE=timestamp, PROFILE_ID=cac_profile),
+    )
     if not path.is_absolute():
         path = constants.STATE_DIR / path
     path = path.resolve()
@@ -104,24 +104,26 @@ def get_artifact_destination_path(
     return path
 
 
-def override_config_with_cli_args(config: configparser.ConfigParser, args: argparse.Namespace) -> None:
-    """
-    Override config with CLI args.
+def override_config_with_cli_args(
+    config: configparser.ConfigParser, args: argparse.Namespace
+) -> None:
+    """Override config with CLI args.
 
     Args:
         config: config parser
         args: CLI args
+
     """
-    # args:command.option -> config:section.option
+    # Define how CLI args map to config options:
+    # (args) command.option -> (config) section.option
     map_cli_paths_to_config = {
         "audit.html_file": "openscap_backend.audit_report",
         "audit.results_file": "openscap_backend.audit_results",
         "generate-fix.output": "openscap_backend.fix_script",
     }
-    for cli_arg in map_cli_paths_to_config:
+    for cli_arg, cfg_opt in map_cli_paths_to_config.items():
         command, option = cli_arg.split(".")
         if args.command == command and hasattr(args, option):
-            config_section, config_option = map_cli_paths_to_config[cli_arg].split(".")
-            new_path = str(getattr(args, option).resolve())
-            config.set(config_section, config_option, new_path)
-
+            config_section, config_option = cfg_opt.split(".")
+            new_path = Path(getattr(args, option).resolve())
+            config.set(config_section, config_option, str(new_path))
--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,8 +1,11 @@
 import json
-import pytest
 from pathlib import Path
-from usg.models import Benchmark, Profile, Benchmarks
-from usg.exceptions import ProfileNotFoundError, BenchmarkError
+
+import pytest
+
+from usg.exceptions import BenchmarkError, ProfileNotFoundError
+from usg.models import Benchmark, Benchmarks, Profile
+
 
 def test_usgbenchmarks(dummy_benchmarks):
     # Test that the two benchmarks from a file are correctly loaded
@@ -13,14 +16,18 @@ def test_usgbenchmarks(dummy_benchmarks):
     assert isinstance(benchmarks["ubuntu2404_CIS_1"], Benchmark)
     assert isinstance(benchmarks["ubuntu2404_STIG_1"], Benchmark)
 
+
 def test_usgbenchmarks_from_missing_benchmarks(tmp_path, dummy_benchmarks):
     # Test that an error is raised if the benchmarks key is missing
     b = json.loads(dummy_benchmarks.read_text())
     b.pop("benchmarks")
     json_file = tmp_path / "benchmarks.json"
     json_file.write_text(json.dumps(b))
-    with pytest.raises(BenchmarkError, match="Invalid '.*' contents. Could not find key 'benchmarks'"):
+    with pytest.raises(
+        BenchmarkError, match="Invalid '.*' contents. Could not find key 'benchmarks'"
+    ):
         Benchmarks.from_json(json_file)
+
 
 def test_usgbenchmarks_from_missing_version(tmp_path, dummy_benchmarks):
     # Test that an error is raised if the version key is missing
@@ -28,8 +35,11 @@ def test_usgbenchmarks_from_missing_version(tmp_path, dummy_benchmarks):
     b.pop("version")
     json_file = tmp_path / "benchmarks.json"
     json_file.write_text(json.dumps(b))
-    with pytest.raises(BenchmarkError, match="Invalid '.*' contents. Could not find key 'version'"):
+    with pytest.raises(
+        BenchmarkError, match="Invalid '.*' contents. Could not find key 'version'"
+    ):
         Benchmarks.from_json(json_file)
+
 
 def test_usgbenchmarks_from_duplicate_benchmark_id(tmp_path, dummy_benchmarks):
     # Test that an error is raised if the version key is missing
@@ -37,8 +47,12 @@ def test_usgbenchmarks_from_duplicate_benchmark_id(tmp_path, dummy_benchmarks):
     b["benchmarks"].append(b["benchmarks"][0])
     json_file = tmp_path / "benchmarks.json"
     json_file.write_text(json.dumps(b))
-    with pytest.raises(BenchmarkError, match="Malformed dataset - duplicate benchmark ID: ubuntu2404_CIS_1"):
+    with pytest.raises(
+        BenchmarkError,
+        match="Malformed dataset - duplicate benchmark ID: ubuntu2404_CIS_1",
+    ):
         Benchmarks.from_json(json_file)
+
 
 def test_usgbenchmark(dummy_benchmarks):
     # Test that the single benchmark from a file is correctly parsed
@@ -50,31 +64,48 @@ def test_usgbenchmark(dummy_benchmarks):
     assert benchmark.product == "ubuntu2404"
     assert benchmark.product_long == "Ubuntu 24.04 LTS (Noble Numbat)"
     assert benchmark.version == "v1.0.1"
-    assert benchmark.description == "ComplianceAsCode implementation of CIS Ubuntu 24.04 LTS v1.0.1 benchmark.\n"
+    assert (
+        benchmark.description
+        == "ComplianceAsCode implementation of CIS Ubuntu 24.04 LTS v1.0.1 benchmark.\n"
+    )
     assert benchmark.compatible_versions == ["v1.0.0.usg1"]
     assert benchmark.breaking_upgrade_path == ["v2.0.0"]
-    assert benchmark.release_notes_url == "https://github.com/canonical/ubuntu-security-guide/"
-    assert benchmark.reference_url == "https://www.cisecurity.org/benchmark/ubuntu_linux/"
+    assert (
+        benchmark.release_notes_url
+        == "https://github.com/canonical/ubuntu-security-guide/"
+    )
+    assert (
+        benchmark.reference_url == "https://www.cisecurity.org/benchmark/ubuntu_linux/"
+    )
     assert benchmark.is_latest == False
     assert len(benchmark.profiles) == 4
     assert benchmark.tailoring_files["cis_level2_server"] == {
-            "file": "ubuntu2404_CIS_2/tailoring/cis_level2_server-tailoring.xml",
-            "sha256": "b1c953719606572f8ab507e9bfbbd570724127e8c87055aca90ebff85817e6f5"
-        }
+        "file": "ubuntu2404_CIS_2/tailoring/cis_level2_server-tailoring.xml",
+        "sha256": "b1c953719606572f8ab507e9bfbbd570724127e8c87055aca90ebff85817e6f5",
+    }
     t_rel_path = benchmark.get_tailoring_file_relative_path("cis_level2_server")
-    assert t_rel_path == Path("ubuntu2404_CIS_2/tailoring/cis_level2_server-tailoring.xml")
+    assert t_rel_path == Path(
+        "ubuntu2404_CIS_2/tailoring/cis_level2_server-tailoring.xml"
+    )
 
     ds_file = benchmark.data_files["datastream_gz"]
     assert ds_file.type == "datastream_gz"
     assert ds_file.rel_path == Path("ubuntu2404_CIS_2/ssg-ubuntu2404-ds.xml.gz")
-    assert ds_file.sha256 == "5ef3b9feb87381e7cca99f50cfaae6d5789ba66e512e97bd82e027f2005a8366"
+    assert (
+        ds_file.sha256
+        == "5ef3b9feb87381e7cca99f50cfaae6d5789ba66e512e97bd82e027f2005a8366"
+    )
+
 
 @pytest.mark.parametrize("missing_key", ["profiles", "data_files", "tailoring_files"])
 def test_usgbenchmark_bad_data(dummy_benchmarks, missing_key):
     # Test that an error is raised if the data is malformed
     raw_data = json.loads(dummy_benchmarks.read_text())
     raw_data["benchmarks"][0].pop(missing_key)
-    with pytest.raises(BenchmarkError, match=f"Failed to create Benchmark object from.*: '{missing_key}"):
+    with pytest.raises(
+        BenchmarkError,
+        match=f"Failed to create Benchmark object from.*: '{missing_key}",
+    ):
         Benchmark.from_dict(raw_data["benchmarks"][0])
 
 
@@ -88,8 +119,12 @@ def test_usgbenchmark_tailoring_bad_profile(dummy_benchmarks):
 def test_usgbenchmark_tailoring_file(dummy_benchmarks):
     benchmarks = Benchmarks.from_json(dummy_benchmarks)
     benchmark = benchmarks["ubuntu2404_CIS_1"]
-    assert benchmark.get_tailoring_file_relative_path("cis_level1_server") == Path("ubuntu2404_CIS_1/tailoring/cis_level1_server-tailoring.xml")
-    assert benchmark.get_tailoring_file_relative_path("cis_level2_server") == Path("ubuntu2404_CIS_1/tailoring/cis_level2_server-tailoring.xml")
+    assert benchmark.get_tailoring_file_relative_path("cis_level1_server") == Path(
+        "ubuntu2404_CIS_1/tailoring/cis_level1_server-tailoring.xml"
+    )
+    assert benchmark.get_tailoring_file_relative_path("cis_level2_server") == Path(
+        "ubuntu2404_CIS_1/tailoring/cis_level2_server-tailoring.xml"
+    )
 
 
 def test_usgbenchmark_profiles(dummy_benchmarks):
@@ -100,8 +135,12 @@ def test_usgbenchmark_profiles(dummy_benchmarks):
         assert isinstance(profile, Profile)
         assert profile.benchmark_id == "ubuntu2404_CIS_1"
         assert profile.profile_id == profile_id
-        assert profile.profile_legacy_id == benchmark.profiles[profile_id].profile_legacy_id
+        assert (
+            profile.profile_legacy_id
+            == benchmark.profiles[profile_id].profile_legacy_id
+        )
         assert profile.tailoring_file is None
+
 
 def test_usgprofile():
     # Test Profile initialization and properties
@@ -109,8 +148,8 @@ def test_usgprofile():
         profile_id="test_profile",
         profile_legacy_id="test_profile_legacy_id",
         benchmark_id="test_benchmark_id",
-        tailoring_file=Path("test_tailoring.xml")
-        )
+        tailoring_file=Path("test_tailoring.xml"),
+    )
     assert profile.profile_id == "test_profile"
     assert profile.profile_legacy_id == "test_profile_legacy_id"
     assert profile.benchmark_id == "test_benchmark_id"
