11420282deb0ab082f2d66e1f8e8d525de5f960e
Miha Purg <miha.purg@canonical.com>
2025-08-26 11:34:50 +0200

    Introduce state file to retain the benchmark version
    The version of the benchmark profile used in usg commands
    is now set during first execution of usg and retained
    until explicitly redefined using the -V/--benchmark-version flag.
    
    During the initial usg run, or if the state file is removed,
    or if using `--benchmark-version "latest"`, the latest version
    of the specified benchmark profile is selected and stored
    in the state file /var/lib/usg/state.json.
    
    This makes the process more aligned with tailoring files which
    are also pinned to a specific version to avoid an unwanted automatic
    upgrade to a backwards incompatible version of the benchmark.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 from pathlib import Path
@@ -9,7 +10,7 @@ from usg import __version__
 from usg.results import AuditResults, BackendArtifacts
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
     # patch the usg module:
     # - set benchmarks to dummy benchmarks fixture
@@ -17,6 +18,8 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
     # - set validate_perms and verify_integrity to no-ops
     # - patch USG with a dummy USG class overriding audit, generate_fix, fix
     # - change working directory to a tmp dir and create tailoring file in it
+    # - patch load state functions to always return "latest" (always clean slate and no state file)
+    #   and set save_state to noop
     mp = MonkeyPatch()
 
     tmp_state_dir = tmp_path_factory.mktemp("var_dir")
@@ -24,6 +27,7 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
 
     mp.setattr(constants_module, "BENCHMARK_METADATA_PATH", dummy_benchmarks)
     mp.setattr(constants_module, "STATE_DIR", tmp_state_dir)
+    mp.setattr(constants_module, "CLI_STATE_FILE", tmp_state_dir / "state.json")
 
     from usg import usg as usg_module
 
@@ -73,6 +77,9 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
 
     mp.setattr(cli_module, "USG", DummyUSG)
     mp.setattr(cli_module.os, "geteuid", lambda: 0)
+    mp.setattr(cli_module, "load_benchmark_version_state", lambda *a: "latest")
+    mp.setattr(cli_module, "save_benchmark_version_state", lambda *a: None)
+
 
     # cd into a tmp dir and create a tailoring file in it
     # (hack to use hardcoded tailoring name in parametrized tests)
@@ -268,13 +275,7 @@ def test_cli_invocation(
             "ubuntu2404_CIS_3",
         ),
         (
-            [
-                "generate-tailoring",
-                "cis_level1_server",
-                "gen-tail-out.xml",
-                "-V",
-                "v1.0.0",
-            ],
+            ["generate-tailoring", "cis_level1_server", "gen-tail-out.xml", "-V", "v1.0.0",],
             "ubuntu2404_CIS_1",
         ),
     ],
@@ -301,13 +302,144 @@ def test_cli_generate_tailoring(
     <select idref="xccdf_org.ssgproject.content_rule_test_rule" selected="true"/>
     </Profile>
 </Tailoring>""")
-
+    
     sys.argv = ["usg", *cli_args]
-    from usg import cli
 
+    from usg import cli
     cli.cli()
     captured = capsys.readouterr()
     assert Path("gen-tail-out.xml").exists()
     assert "generate-tailoring command completed" in captured.out
     os.remove(tailoring_file)
     os.remove("gen-tail-out.xml")
+
+
+def test_load_benchmark_version_state(tmp_path, monkeypatch):
+    # Test that the benchmark version is correctly loaded from the state file
+    from usg import constants as constants_module
+    from usg.cli import load_benchmark_version_state
+
+    state_file = tmp_path / "state.json"
+    monkeypatch.setattr(constants_module, "CLI_STATE_FILE", state_file)
+
+    # missing file should default to "latest" version
+    assert load_benchmark_version_state("cis_level1_server") == "latest"
+
+    Path(state_file).write_text(json.dumps({
+        "benchmark_versions": {
+            "cis_level1_server": "test_version"
+        }
+    }))
+    assert load_benchmark_version_state("cis_level1_server") == "test_version"
+    
+    # missing benchmark profile defaults to "latest" version
+    assert load_benchmark_version_state("missing_profile") == "latest"
+
+
+def test_load_benchmark_version_state_error(tmp_path, monkeypatch):
+    # Test that corrupt/unreadable state file results in error
+    from usg import constants as constants_module
+    from usg.cli import load_benchmark_version_state, StateFileError
+
+    state_file = tmp_path / "state.json"
+    monkeypatch.setattr(constants_module, "CLI_STATE_FILE", state_file)
+
+    # non-json
+    Path(state_file).write_text("test")
+    with pytest.raises(StateFileError, match="Corrupted"):
+        load_benchmark_version_state("cis_level1_server")
+    
+    # bad data
+    Path(state_file).write_text(json.dumps({
+        "bad_key_for_benchmark_versions": {
+            "cis_level1_server": "test_version"
+        }
+    }))
+    with pytest.raises(StateFileError, match="Corrupted"):
+        load_benchmark_version_state("cis_level1_server")
+   
+
+def test_save_benchmark_version_state(tmp_path, monkeypatch):
+    # Test that benchmark version state is stored correctly to state file
+    from usg import constants as constants_module
+    from usg.cli import save_benchmark_version_state, load_benchmark_version_state
+
+    state_file = tmp_path / "state.json"
+    monkeypatch.setattr(constants_module, "CLI_STATE_FILE", state_file)
+
+    # non-existing file should be created
+    Path(state_file).unlink(missing_ok=True)
+    save_benchmark_version_state("cis_level1_server", "test_version")
+    assert load_benchmark_version_state("cis_level1_server") == "test_version"
+
+    # existing file should be updated
+    save_benchmark_version_state("cis_level1_server", "test_version2")
+    assert load_benchmark_version_state("cis_level1_server") == "test_version2"
+
+
+def test_save_benchmark_version_state_error(tmp_path, monkeypatch):
+    # Test that benchmark version state is stored correctly to state file
+    from usg import constants as constants_module
+    from usg.cli import save_benchmark_version_state, load_benchmark_version_state, StateFileError
+
+    state_file = tmp_path / "state.json"
+    monkeypatch.setattr(constants_module, "CLI_STATE_FILE", state_file)
+
+    # corrupt state data
+    Path(state_file).write_text("asd")
+    with pytest.raises(StateFileError, match="Corrupted"):
+        save_benchmark_version_state("cis_level1_server", "test_version")
+    
+    # permission error should result in statefileerror
+    Path(state_file).write_text(json.dumps({
+        "benchmark_versions": {
+            "cis_level1_server": "test_version"
+        }
+    }))
+    tmp_path.chmod(0o500) # prevent moving the state tmpfile to final dest
+    with pytest.raises(StateFileError, match="Failed to write to state file"):
+        save_benchmark_version_state("cis_level1_server", "test_version2")
+
+
+def test_benchmark_version_state_integration(patch_usg_and_cli, capsys, monkeypatch, tmp_path):
+    # test that cli commands retain benchmark version across runs
+     
+     # hack to undo monkepatches to state functions done in patch_usg_and_cli
+    from usg import cli
+    from usg.cli import USG as DummyUSG
+    import importlib
+    importlib.reload(cli)
+    monkeypatch.setattr(cli, "USG", DummyUSG)
+    monkeypatch.setattr(cli.os, "geteuid", lambda: 0)
+
+    from usg.cli import load_benchmark_version_state
+
+    # no args switches initializes to "latest", which is "v2.0.0" in test benchmarks
+    sys.argv = ["usg", "audit", "cis_level1_server"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v2.0.0"
+
+    # re-set to v1.0.1
+    sys.argv = ["usg", "audit", "cis_level1_server", "-V", "v1.0.1"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
+
+    # no args reuses v1.0.1
+    sys.argv = ["usg", "audit", "cis_level1_server"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
+
+    # confirm with fix
+    sys.argv = ["usg", "fix", "cis_level1_server"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
+
+    # confirm with generate-fix
+    sys.argv = ["usg", "generate-fix", "cis_level1_server"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
+
+    # re-set to latest
+    sys.argv = ["usg", "generate-fix", "cis_level1_server", "-V", "latest"]
+    cli.cli()
+    assert load_benchmark_version_state("cis_level1_server") == "v2.0.0"
