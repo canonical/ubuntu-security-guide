7695a93975b7046d6730ff126dfe95b0b7a31171
Miha Purg <miha.purg@canonical.com>
2025-09-30 13:05:37 +0200

    Add license headers to source files

--- tools/build.py
+++ tools/build.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 #
 # Ubuntu Security Guide
-# Copyright (C) 2022 Canonical Limited
+# Copyright (C) 2025 Canonical Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-only
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# but WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
 import sys
 
--- tools/create_rule_and_variable_doc.py
+++ tools/create_rule_and_variable_doc.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-"""Functions for populating rule and variable markdown files."""
 #
-# Ubuntu Security Guides
-# Copyright (C) 2025 Canonical Limited
+# Ubuntu Security Guide
+# Copyright (C) 2025 Canonical Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-only
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# but WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
+"""Functions for populating rule and variable markdown files."""
 import sys
 
 if sys.version_info < (3,12):
