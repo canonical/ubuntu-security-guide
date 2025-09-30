2a1d6c2858cf56622daa085bb917e8bf1ad27fa9
Miha Purg <miha.purg@canonical.com>
2025-09-30 13:05:37 +0200

    Add license headers to source files

--- tools/cac_tools.py
+++ tools/cac_tools.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-"""Common functionality for reading ComplianceAsCode profiles/controls."""
 #
 # Ubuntu Security Guide
-# Copyright (C) 2025 Canonical Limited
+# Copyright (C) 2025 Canonical Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-only
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# but WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
+"""Common functionality for reading ComplianceAsCode profiles/controls."""
 import sys
 
 if sys.version_info < (3,12):
