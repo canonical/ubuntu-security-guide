b7008b3f17ee47997337b8dfcc5b96644cd77e26
Miha Purg <miha.purg@canonical.com>
2025-10-15 16:35:42 +0200

    Default to initial profiles instead of latest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,7 +26,7 @@ import pytest
 from pytest import MonkeyPatch
 
 from usg import cli, exceptions, results, constants, utils, __version__
-from usg.cli import init_logging, load_benchmark_version_state, save_benchmark_version_state
+from usg.cli import init_logging
 from usg.exceptions import LockError, StateFileError, USGError
 from usg.usg import USG
 from usg.results import AuditResults, BackendArtifacts
@@ -42,7 +42,6 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
     # - set check_perms, verify_integrity, and acquire_lock to no-ops
     # - patch USG with a dummy USG class overriding audit, generate_fix, fix
     # - change working directory to a tmp dir and create tailoring file in it
-    # - patch load state functions to always return "latest" (always clean slate and no state file)
     #   and set save_state to noop
     mp = MonkeyPatch()
 
@@ -102,8 +101,6 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
 
     mp.setattr(cli, "USG", DummyUSG)
     mp.setattr(os, "geteuid", lambda: 0)
-    mp.setattr(cli, "load_benchmark_version_state", lambda *a: "latest")
-    mp.setattr(cli, "save_benchmark_version_state", lambda *a: None)
 
 
     # cd into a tmp dir and create a tailoring file in it
@@ -113,7 +110,7 @@ def patch_usg_and_cli(tmp_path_factory, dummy_benchmarks):
     tailoring_file = test_dir / "tailoring.xml"
     tailoring_xml = """<?xml version="1.0" encoding="UTF-8"?>
 <Tailoring xmlns="http://checklists.nist.gov/xccdf/1.2">
-  <benchmark href="/usr/share/usg-benchmarks/ubuntu2404_CIS_1"/>
+  <benchmark href="/usr/share/usg-benchmarks/ubuntu2404_CIS_2"/>
   <Profile id="tailored_profile">
     <select idref="xccdf_org.ssgproject.content_rule_test_rule" selected="true"/>
   </Profile>
@@ -183,26 +180,26 @@ def test_usg_init_error(patch_usg_and_cli, monkeypatch, capsys):
         (["--version"], __version__.replace(".4.", ".04."), None, 0),
         (["notacommand"], None, "invalid choice:", 2),
         # list command
-        (["list"], "Listing available profiles...", None, None),
+        (["list"], "Listing latest and default profiles", None, None),
         (["list", "asd"], None, "unrecognized arguments: asd", 2),
         (["list", "--all"], "v1.0.0", None, None),
         (["list", "--machine-readable"], "cis_level1_server:CIS:ubuntu2404:v2.0.0", None, None),
         (["list"], "v2.0.0", None, None),
         # successful commands with profile argument
-        (["info", "cis_level1_server"], "ubuntu2404_CIS_3", None, None),
-        (["audit", "cis_level1_server"], "ubuntu2404_CIS_3", None, None),
-        (["fix", "cis_level1_server"], "ubuntu2404_CIS_3", None, None),
-        (["generate-fix", "cis_level1_server"], "ubuntu2404_CIS_3", None, None),
+        (["info", "cis_level1_server"], "ubuntu2404_CIS_1", None, None),
+        (["audit", "cis_level1_server"], "ubuntu2404_CIS_1", None, None),
+        (["fix", "cis_level1_server"], "ubuntu2404_CIS_1", None, None),
+        (["generate-fix", "cis_level1_server"], "ubuntu2404_CIS_1", None, None),
         # successful commands with profile and version
-        (["info", "cis_level1_server", "-b", "v1.0.0"], "ubuntu2404_CIS_1", None, None),
-        (["audit", "cis_level1_server", "-b", "v1.0.0"], "ubuntu2404_CIS_1", None, None),
-        (["fix", "cis_level1_server", "-b", "v1.0.0"], "ubuntu2404_CIS_1", None, None),
-        (["generate-fix", "cis_level1_server", "-b", "v1.0.0"], "ubuntu2404_CIS_1", None, None),
+        (["info", "cis_level1_server", "-b", "v2.0.0"], "ubuntu2404_CIS_3", None, None),
+        (["audit", "cis_level1_server", "-b", "v2.0.0"], "ubuntu2404_CIS_3", None, None),
+        (["fix", "cis_level1_server", "-b", "v2.0.0"], "ubuntu2404_CIS_3", None, None),
+        (["generate-fix", "cis_level1_server", "-b", "v2.0.0"], "ubuntu2404_CIS_3", None, None),
         # successful commands with tailoring file
-        (["info", "-t", "tailoring.xml"], "ubuntu2404_CIS_1", None, None),
-        (["audit", "-t", "tailoring.xml"], "ubuntu2404_CIS_1", None, None),
-        (["fix", "-t", "tailoring.xml"], "ubuntu2404_CIS_1", None, None),
-        (["generate-fix", "-t", "tailoring.xml"], "ubuntu2404_CIS_1", None, None),
+        (["info", "-t", "tailoring.xml"], "ubuntu2404_CIS_2", None, None),
+        (["audit", "-t", "tailoring.xml"], "ubuntu2404_CIS_2", None, None),
+        (["fix", "-t", "tailoring.xml"], "ubuntu2404_CIS_2", None, None),
+        (["generate-fix", "-t", "tailoring.xml"], "ubuntu2404_CIS_2", None, None),
         # failed commands without a profile or a tailoring file
         (["info"], None, "Error: a profile or a tailoring file must be provided.", 2),
         (["audit"], None, "Error: a profile or a tailoring file must be provided.", 2),
@@ -324,117 +321,6 @@ def test_cli_default_tailoring_file(patch_usg_and_cli, monkeypatch, capsys, capl
     assert "tailored_profile" in captured.out
 
 
-def test_load_benchmark_version_state(tmp_path, monkeypatch):
-    # Test that the benchmark version is correctly loaded from the state file
-    state_file = tmp_path / "state.json"
-    monkeypatch.setattr(constants, "CLI_STATE_FILE", state_file)
-
-    # missing file should default to "latest" version
-    assert load_benchmark_version_state("cis_level1_server") == "latest"
-
-    Path(state_file).write_text(json.dumps({
-        "benchmark_versions": {
-            "cis_level1_server": "test_version"
-        }
-    }))
-    assert load_benchmark_version_state("cis_level1_server") == "test_version"
-
-    # missing benchmark profile defaults to "latest" version
-    assert load_benchmark_version_state("missing_profile") == "latest"
-
-
-def test_load_benchmark_version_state_error(tmp_path, monkeypatch):
-    # Test that corrupt/unreadable state file results in error
-    state_file = tmp_path / "state.json"
-    monkeypatch.setattr(constants, "CLI_STATE_FILE", state_file)
-
-    # non-json
-    Path(state_file).write_text("test")
-    with pytest.raises(StateFileError, match="Corrupted"):
-        load_benchmark_version_state("cis_level1_server")
-
-    # bad data
-    Path(state_file).write_text(json.dumps({
-        "bad_key_for_benchmark_versions": {
-            "cis_level1_server": "test_version"
-        }
-    }))
-    with pytest.raises(StateFileError, match="Corrupted"):
-        load_benchmark_version_state("cis_level1_server")
-
-
-def test_save_benchmark_version_state(tmp_path, monkeypatch):
-    # Test that benchmark version state is stored correctly to state file
-    state_file = tmp_path / "state.json"
-    monkeypatch.setattr(constants, "CLI_STATE_FILE", state_file)
-
-    # non-existing file should be created
-    Path(state_file).unlink(missing_ok=True)
-    save_benchmark_version_state("cis_level1_server", "test_version")
-    assert load_benchmark_version_state("cis_level1_server") == "test_version"
-
-    # existing file should be updated
-    save_benchmark_version_state("cis_level1_server", "test_version2")
-    assert load_benchmark_version_state("cis_level1_server") == "test_version2"
-
-
-def test_save_benchmark_version_state_corrupted_error(tmp_path, monkeypatch):
-    # Test that corrupted state file raises error
-    state_file = tmp_path / "state.json"
-    monkeypatch.setattr(constants, "CLI_STATE_FILE", state_file)
-
-    # corrupt state data
-    Path(state_file).write_text("asd")
-    with pytest.raises(StateFileError, match="Corrupted"):
-        save_benchmark_version_state("cis_level1_server", "test_version")
-
-
-def test_save_benchmark_version_state_write_error(tmp_path, monkeypatch):
-    # Test that permission failure when saving state file raises error
-    monkeypatch.setattr(constants, "CLI_STATE_FILE", "/dev/null/notwritable/usg.json")
-    with pytest.raises(StateFileError, match="Failed to write to state file"):
-        save_benchmark_version_state("cis_level1_server", "test_version2")
-
-
-def test_benchmark_version_state_integration(patch_usg_and_cli, capsys, monkeypatch, tmp_path):
-    # test that cli commands retain benchmark version across runs
-
-    # hack to undo monkepatches to load/save_state functions done in patch_usg_and_cli
-    from usg.cli import USG as DummyUSG
-    importlib.reload(cli)
-    monkeypatch.setattr(cli, "USG", DummyUSG)
-    monkeypatch.setattr(cli.os, "geteuid", lambda: 0)
-    monkeypatch.setattr(cli, "acquire_lock", lambda: None)
-
-    # no args switches initializes to "latest", which is "v2.0.0" in test benchmarks
-    sys.argv = ["usg", "audit", "cis_level1_server"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v2.0.0"
-
-    # re-set to v1.0.1
-    sys.argv = ["usg", "audit", "cis_level1_server", "-b", "v1.0.1"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
-
-    # no args reuses v1.0.1
-    sys.argv = ["usg", "audit", "cis_level1_server"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
-
-    # confirm with fix
-    sys.argv = ["usg", "fix", "cis_level1_server"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
-
-    # confirm with generate-fix
-    sys.argv = ["usg", "generate-fix", "cis_level1_server"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v1.0.1"
-
-    # re-set to latest
-    sys.argv = ["usg", "generate-fix", "cis_level1_server", "--benchmark-version", "latest"]
-    cli.cli()
-    assert load_benchmark_version_state("cis_level1_server") == "v2.0.0"
 
 
 def test_init_logging(capsys, tmp_path):
